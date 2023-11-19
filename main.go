427761771fa5a7833b07acd84d7d3cb66e48772f
David Baker <dpbaker1298@gmail.com>
2023-11-19 12:32:26 -0500

    Base project structure with functions. Referencing our Lucid Chart project plan.

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"encoding/xml"
+	"io/ioutil"
+	"os"
+	"net/http"
+)
+
+func main() {
+	readGoogleTrends()
+}
+
+func readGoogleTrends() {
+	getGoogleTrends()
+}
+
+func getGoogleTrends() {}
